1abf4cfabe4f4fb784c2c840a1b9aa23ae123e04
SimplyJarvis <james.jarvis@students.plymouth.ac.uk>
2017-09-30 18:41:57 +0100

    Initial Experiemnt for Twitch Chat API
    Twitch Chat API Integration

---
+++ Assets/Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TwitchIRC))]
+public class Test : MonoBehaviour {
+
+    private TwitchIRC IRC;
+    private LinkedList<GameObject> messages = new LinkedList<GameObject>();
+    public int maxMessages = 100;
+    public Text test;
+
+    // Use this for initialization
+    void Start () {
+        IRC = this.GetComponent<TwitchIRC>();
+        //IRC.SendCommand("CAP REQ :twitch.tv/tags"); //register for additional data such as emote-ids, name color etc.
+        IRC.messageRecievedEvent.AddListener(OnChatMsgRecieved);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnChatMsgRecieved(string msg)
+    {
+        //parse from buffer.
+        int msgIndex = msg.IndexOf("PRIVMSG #");
+        string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
+        string user = msg.Substring(1, msg.IndexOf('!') - 1);
+
+        //remove old messages for performance reasons.
+        if (messages.Count > maxMessages)
+        {
+            Destroy(messages.First.Value);
+            messages.RemoveFirst();
+        }
+
+        test.text = user;
+        Debug.Log(user + " : " + msgString);
+
+        ////add new message.
+        //CreateUIMessage(user, msgString);
+    }
+}
