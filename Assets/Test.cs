ea6e8be244c02644f265ce96784170b0c937bb61
Steeldarkeagel <steeldarkeagel@live.com>
2017-10-03 14:53:18 +0100

    Cube games
    Move cube. Colour cube

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -12,6 +12,7 @@ public class Test : MonoBehaviour {
     public Text test;
     public Text test2;
     private bool notWon = false;
+    public GameObject boxey;
 
     // Use this for initialization
     void Start () {
@@ -39,6 +40,7 @@ public class Test : MonoBehaviour {
             Destroy(messages.First.Value);
             messages.RemoveFirst();
         }
+        //text game
         if (user == "simply_jarvis" & msgString == "!start")
         {
             test.enabled = true;
@@ -49,7 +51,44 @@ public class Test : MonoBehaviour {
             test2.text = "Winner: " + user;
             notWon = true;
         }
+        //end text game
+
+        //move cube
+        if (msgString == "Move left")
+        {
+            Vector3 position = boxey.transform.position;
+            position.x += -1;
+            boxey.transform.position = position;
+        }
+        else if (msgString == "Move right")
+        {
+            Vector3 position = boxey.transform.position;
+            position.x += 1;
+            boxey.transform.position = position;
+        }
+        else if (msgString == "Move down")
+        {
+            Vector3 position = boxey.transform.position;
+            position.y += -1;
+            boxey.transform.position = position;
+        }
+        else if (msgString == "Move up")
+        {
+            Vector3 position = boxey.transform.position;
+            position.y += 1;
+            boxey.transform.position = position;
+        }
+        //end move cube
+
+        //colour cube
+        if (msgString.Contains("!cubecolour"))
+        {
+            boxey.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        }
+        //end colour cube
+
         //test.text = user;
+
         Debug.Log(user + " : " + msgString);
 
         ////add new message.
