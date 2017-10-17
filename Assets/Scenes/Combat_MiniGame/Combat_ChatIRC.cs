2b63deb7291043f003c03da5fe4eec444ad473e7
SimplyJarvis <james.jarvis@students.plymouth.ac.uk>
2017-10-17 11:56:11 +0100

    Added Combat MiniGame
    Development Started

---
+++ Assets/Scenes/Combat_MiniGame/Combat_ChatIRC.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TwitchIRC))]
+public class Combat_ChatIRC : MonoBehaviour {
+
+    private TwitchIRC IRC;
+    private LinkedList<GameObject> messages = new LinkedList<GameObject>();
+    public int maxMessages = 100;
+    private bool StartingPhase = true;
+    [SerializeField]
+    private GameObject playerPrefab;
+    private string[] CurrentPlayers;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnChatMsgRecieved(string msg)
+    {
+        //parse from buffer.
+        int msgIndex = msg.IndexOf("PRIVMSG #");
+        string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
+        string user = msg.Substring(1, msg.IndexOf('!') - 1);
+
+        //remove old messages for performance reasons.
+        if (messages.Count > maxMessages)
+        {
+            Destroy(messages.First.Value);
+            messages.RemoveFirst();
+        }
+
+        if (StartingPhase == true)
+        {
+
+        }
+
+    }
+}
