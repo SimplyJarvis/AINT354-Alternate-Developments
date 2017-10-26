6872e189ef93966ef0e9cb19e0faf963192fe23b
Steeldarkeagel <steeldarkeagel@gmail.com>
2017-10-26 13:27:23 +0100

    Change Scene
    Ability to change scenes added

---
+++ Assets/Shared_Scripts/SwitchGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchGame : MonoBehaviour {
+
+    private TwitchIRC IRC;
+    private LinkedList<GameObject> messages = new LinkedList<GameObject>();
+    public int maxMessages = 100;
+    private string[] sceneNumber;
+
+    // Use this for initialization
+    void Start () {
+        IRC = GameObject.Find("Twitch").GetComponent<TwitchIRC>();
+        IRC.messageRecievedEvent.AddListener(OnChatMsgRecieved);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnChatMsgRecieved(string msg)
+    {
+        //parse from buffer.
+        int msgIndex = msg.IndexOf("PRIVMSG #");
+        string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
+        string user = msg.Substring(1, msg.IndexOf('!') - 1);
+
+        //remove old messages for performance reasons.
+        if (messages.Count > maxMessages)
+        {
+            Destroy(messages.First.Value);
+            messages.RemoveFirst();
+        }
+        if (user == "minigamesontwitch" || user == "steeldarkeagel" || user == "simply_jarvis"|| user == "dafalio")
+        {
+            if (msgString.Contains("!ChangeGame."))
+            {
+                sceneNumber = msgString.Split('.');
+                SceneManager.LoadScene(int.Parse(sceneNumber[1]));
+            }
+        }
+
+    }
+}
