ba54d2b587b4c57c6131de27b3a88336eb705a64
Steeldarkeagel <steeldarkeagel@gmail.com>
2017-10-26 14:42:28 +0100

    Update catchphrase
    Added images
    Commented out dowload

--- Assets/Scenes/Catchphrase/Show_Pics.cs
+++ Assets/Scenes/Catchphrase/Show_Pics.cs
@@ -7,23 +7,31 @@ public class Show_Pics : MonoBehaviour {
 
     public Material image;
     private Image toChange;
-    string url = "https://raw.githubusercontent.com/moecube/ygopro-images/master/pics/10000010.jpg";
+    public Texture[] memes = new Texture[21];
 
-    // Use this for initialization
-    IEnumerator Start () {
-        Texture2D tex;
-        tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
-        WWW www = new WWW(url);
-        yield return www;
-        //Sprite sprites = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
-        //toChange = image.GetComponent<Image>();
-        //toChange.sprite = sprites;
-        www.LoadImageIntoTexture(tex);
-        GetComponent<Renderer>().material.mainTexture = tex;
+
+    // Used for downloading images
+    //string url = "https://raw.githubusercontent.com/moecube/ygopro-images/master/pics/10000020.jpg";        
+    //IEnumerator Start () {
+    //    Texture2D tex;
+    //    tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+    //    WWW www = new WWW(url);
+    //    yield return www;
+    //    www.LoadImageIntoTexture(tex);
+    //    GetComponent<Image>().material.mainTexture = tex;
+    //    GetComponent<Image>().enabled = false;
+    //    GetComponent<Image>().enabled = true;
+    //}
+
+    void Start()
+    {
+        GetComponent<Image>().material.mainTexture = memes[Random.Range(0, 20)];
+        GetComponent<Image>().enabled = false;
+        GetComponent<Image>().enabled = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         
     }
     
