d75deb43b0ed31d693ab513126f319e1b52eded0
David Fall <David.fall@students.plymouth.ac.uk>
2017-11-29 00:09:23 +0000

    added a tonne of models all animated
    added models for the horse racing game

---
+++ Assets/Scenes/Horse Racing/Models/Maze/Scripts/Player.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private MazeCell currentCell;
+
+	private MazeDirection currentDirection;
+    private TwitchIRC IRC;
+    private LinkedList<GameObject> messages = new LinkedList<GameObject>();
+    public int maxMessages = 100;
+
+    private void Start()
+    {
+        IRC = GameObject.Find("Twitch").GetComponent<TwitchIRC>();
+        IRC.messageRecievedEvent.AddListener(OnChatMsgRecieved);
+    }
+
+    public void SetLocation (MazeCell cell) {
+		if (currentCell != null) {
+			currentCell.OnPlayerExited();
+		}
+		currentCell = cell;
+		transform.localPosition = cell.transform.localPosition;
+		currentCell.OnPlayerEntered();
+	}
+
+	private void Move (MazeDirection direction) {
+		MazeCellEdge edge = currentCell.GetEdge(direction);
+		if (edge is MazePassage) {
+			SetLocation(edge.otherCell);
+		}
+	}
+
+	private void Look (MazeDirection direction) {
+		transform.localRotation = direction.ToRotation();
+		currentDirection = direction;
+	}
+    void OnChatMsgRecieved(string msg)
+    {
+        //parse from buffer.
+        int msgIndex = msg.IndexOf("PRIVMSG #");
+        string msgString = msg.Substring(msgIndex + IRC.channelName.Length + 11);
+        string user = msg.Substring(1, msg.IndexOf('!') - 1);
+
+        //remove old messages for performance reasons.
+        if (messages.Count > maxMessages)
+        {
+            Destroy(messages.First.Value);
+            messages.RemoveFirst();
+        }
+
+        if (msgString == "forward")
+        {
+            Move(currentDirection);
+        }
+        else if (msgString == "right")
+        {
+            Move(currentDirection.GetNextClockwise());
+        }
+        else if (msgString == "back")
+        {
+            Move(currentDirection.GetOpposite());
+        }
+        else if (msgString == "left")
+        {
+            Move(currentDirection.GetNextCounterclockwise());
+        }
+        else if (msgString == "cw")
+        {
+            Look(currentDirection.GetNextCounterclockwise());
+        }
+        else if (msgString == "ccw")
+        {
+            Look(currentDirection.GetNextClockwise());
+        }
+
+
+
+    }
+
+    private void Update () {
+		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+			Move(currentDirection);
+		}
+		else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+			Move(currentDirection.GetNextClockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+			Move(currentDirection.GetOpposite());
+		}
+		else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+			Move(currentDirection.GetNextCounterclockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.Q)) {
+			Look(currentDirection.GetNextCounterclockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.E)) {
+			Look(currentDirection.GetNextClockwise());
+		}
+	}
+}
---
+++ Assets/Scenes/Horse Racing/Models/Maze/Scripts/WinScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour {
+
+    public GameObject win;
+
+    private void Start()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            win.SetActive(true);
+            Instantiate(win);
+            Debug.Log("winner");
+            Time.timeScale = (0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        win.SetActive(false);
+        Destroy(win);
+        Debug.Log("RunAway");
+        Time.timeScale = (1);
+    }
+
+}
