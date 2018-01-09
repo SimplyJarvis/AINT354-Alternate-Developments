44e1c7b3a80575e964a6a42eb4644c98bd56d81c
Steeldarkeagel <steeldarkeagel@live.com>
2018-01-09 13:13:29 +0000

    Tank
    Power ups added.

---
+++ Assets/Scenes/Combat_MiniGame/SheildScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheildScript : MonoBehaviour {
+
+    private GameObject tank;
+
+	// Use this for initialization
+	void Start () {        
+        tank = GameObject.Find("sheildTank");
+        this.transform.parent = tank.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(tank.transform.position, Vector3.up, 100f * Time.deltaTime);
+	}
+}
---
+++ Assets/Scenes/Combat_MiniGame/powerupType.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerupType : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int powerUp = Random.Range(1, 4);
+        Renderer rend = GetComponent<Renderer>();
+        if (powerUp == 1)
+        {
+            rend.material.color = Color.blue;
+            rend.material.SetColor("_EmissionColor", Color.blue);
+        }
+        if (powerUp == 2)
+        {
+            rend.material.color = Color.red;
+            rend.material.SetColor("_EmissionColor", Color.red);
+        }
+        if (powerUp == 3)
+        {
+            rend.material.color = Color.green;
+            rend.material.SetColor("_EmissionColor", Color.green);
+        }
+        transform.gameObject.tag = "pUp"+powerUp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
