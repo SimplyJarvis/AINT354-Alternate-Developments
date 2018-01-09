534e7da0bd92e7fcbb0458f77b09391d11ff86ac
SimplyJarvis <james.jarvis@students.plymouth.ac.uk>
2018-01-09 11:40:22 +0000

    Horse Racing Intro and AI
    Ai now follows new paths
    Racers are introduced

--- Assets/Scenes/Horse Racing/CheckPoints.cs
+++ Assets/Scenes/Horse Racing/CheckPoints.cs
@@ -6,12 +6,12 @@ using UnityEngine.AI;
 public class CheckPoints : MonoBehaviour {
 
     [SerializeField]
-    private Transform newPos;
+    private Transform[] newPos;
 
 	void OnTriggerEnter(Collider col) {
         if (col.gameObject.tag == "Racer")
         {
-            col.gameObject.GetComponent<NavTarget>().target = newPos;
+            col.gameObject.GetComponent<NavTarget>().target = newPos[Random.Range(0,2)];
             col.gameObject.GetComponent<NavMeshAgent>().speed = Random.Range(3.5f, 5.5f);
         }
 
